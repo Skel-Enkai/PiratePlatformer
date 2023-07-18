58ca97b8e7a41995d5627a4af7f31718ced84fd9
Skel-Enkai <williamwarrenmeeks@gmail.com>
2023-07-18 22:08:39 +0000

    Added Asyncio; so game can be converted to bytecode to run on my webserver.

--- code/main.py
+++ code/main.py
@@ -1,7 +1,10 @@
-import pygame
+import asyncio
 import sys
-from settings import *
+
+import pygame
+
 from game import Game
+from settings import *
 
 # pygame.FULLSCREEN | pygame.SCALED (flags for fullscreen) # (vsync sets fps max to 60)
 # Pygame setup
@@ -11,47 +14,51 @@ screen_surface = pygame.Surface((screen_width, screen_height))
 
 clock = pygame.time.Clock()
 game = Game(screen_surface)
-joysticks = [None]
-current_controller = 0
-
 pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN, pygame.JOYDEVICEADDED,
                          pygame.JOYDEVICEREMOVED, pygame.JOYBUTTONDOWN])
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
 
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+
+async def main():
+    joysticks = [None]
+    current_controller = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            game.controller = False
 
-        elif event.type == game.switch_overworld:
-            game.overworld.wait = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                game.controller = False
 
-        elif game.level:
-            if event.type == game.level.player.sprite.attack_timer:
-                game.level.player.sprite.can_attack = True
+            elif event.type == game.switch_overworld:
+                game.overworld.wait = False
 
-        elif event.type == pygame.JOYDEVICEADDED or event.type == pygame.JOYDEVICEREMOVED:
-            joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+            elif game.level:
+                if event.type == game.level.player.sprite.attack_timer:
+                    game.level.player.sprite.can_attack = True
 
-        elif event.type == pygame.JOYBUTTONDOWN:
-            game.controller = True
-            current_controller = event.joy
-            print(event)
+            elif event.type == pygame.JOYDEVICEADDED or event.type == pygame.JOYDEVICEREMOVED:
+                joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 
-    game.run(joysticks[current_controller])
+            elif event.type == pygame.JOYBUTTONDOWN:
+                game.controller = True
+                current_controller = event.joy
+                print(event)
 
-    screen.blit(screen_surface, (0, 0))
-    pygame.display.update()
+        game.run(joysticks[current_controller])
 
-    clock.tick(60)
-    fps = clock.get_fps()
-    if fps:
-        pass
-        # print(fps)
+        screen.blit(screen_surface, (0, 0))
+        pygame.display.update()
 
+        clock.tick(60)
+        # fps = clock.get_fps()
+        # if fps:
+        #     pass
+        #     # print(fps)
+        await asyncio.sleep(0)
+
+asyncio.run(main())
 # Setup Scalable Resolutions
