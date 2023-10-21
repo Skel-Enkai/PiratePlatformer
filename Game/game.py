f2c46594b4fdc3b185cf87a9f4451ab700495d87
Skel-Enkai <williamwarrenmeeks@gmail.com>
2023-10-21 15:57:55 +0000

    Added Sound Pause Function on Button M.
    Signed-off-by: Skel-Enkai <williamwarrenmeeks@gmail.com>

--- Game/game.py
+++ Game/game.py
@@ -8,6 +8,7 @@ from ui import UI
 class Game:
     def __init__(self, surface):
         # game attributes
+        self.input_wait = False
         self.level = None
         self.current_level = 0
         self.max_level = 5
@@ -34,6 +35,9 @@ class Game:
         self.channel_effects.set_volume(0.3)
         self.hit_sound = pygame.mixer.Sound('./audio/effects/hit.wav')
 
+        self.mute_flag = False
+        self.menu_wait = pygame.event.custom_type()
+
         # overworld creation
         self.overworld = Overworld(self.screen_surface, self.create_level, 0, self.max_level)
         self.status = 'overworld'
@@ -44,9 +48,11 @@ class Game:
     def create_level(self, current_level):
         self.current_level = current_level
         self.level = Level(current_level, self.screen_surface, self.create_overworld,
-                           self.change_coins, self.change_cur_health)
+                           self.change_coins, self.change_cur_health, self.mute_flag)
         self.status = 'level'
         self.music.play(self.level_music, loops=-1)
+        if self.mute_flag:
+            pygame.mixer.pause()
 
     def create_overworld(self, new_max_level):
         if new_max_level > self.max_level:
@@ -55,6 +61,8 @@ class Game:
         self.status = 'overworld'
         pygame.time.set_timer(self.switch_overworld, 1200)
         self.music.play(self.overworld_music, loops=-1)
+        if self.mute_flag:
+            pygame.mixer.pause()
 
     def change_coins(self, amount):
         self.coins += amount
@@ -73,7 +81,26 @@ class Game:
             self.status = 'overworld'
             self.music.play(self.overworld_music, loops=-1)
 
+    def check_menu(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_m] and not self.input_wait:
+            pygame.time.set_timer(self.menu_wait, 600)
+            self.input_wait = True
+            if not self.mute_flag:
+                pygame.mixer.pause()
+                self.mute_flag = True
+                if self.level:
+                    self.level.effects_channel.set_volume(0.0)
+                    self.level.player.sprite.channel.set_volume(0.0)
+            else:
+                self.mute_flag = False
+                pygame.mixer.unpause()
+                if self.level:
+                    self.level.effects_channel.set_volume(0.2)
+                    self.level.player.sprite.channel.set_volume(0.2)
+
     def run(self, joystick=None):
+        self.check_menu()
         if self.status == 'overworld':
             self.overworld.run(joystick, self.controller)
         elif self.status == 'level':
